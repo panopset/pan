3573bdcb2d691b764df0d526bd2170f8c37a75fd
Karl Dinwiddie <karldinwiddie@gmail.com>
2024-03-04 04:50:54 -0800

    .

---
+++ docs/platforms/win/setenv.cmd
@@ -0,0 +1,5 @@
+set JAVA_HOME=%USERPROFILE%\apps\jdk
+set MAVEN_HOME=%USERPROFILE%\apps\mvn
+set GRADLE_HOME=%USERPROFILE%\apps\gradle
+set ANT_HOME=%USERPROFILE%\apps\ant
+set PATH=%JAVA_HOME%\bin;%MAVEN_HOME%\bin;%GRADLE_HOME%\bin;%ANT_HOME%\bin;%PATH%
