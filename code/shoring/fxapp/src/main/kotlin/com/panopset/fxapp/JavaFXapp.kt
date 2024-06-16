414f365878b1031847de87dc4be8445455826091
Karl Dinwiddie <karldinwiddie@gmail.com>
2024-06-16 08:56:58 -0700

    .

--- code/shoring/fxapp/src/main/kotlin/com/panopset/fxapp/JavaFXapp.kt
+++ code/shoring/fxapp/src/main/kotlin/com/panopset/fxapp/JavaFXapp.kt
@@ -137,10 +137,24 @@ object JavaFXapp {
         alert.headerText = DeskApp4XFactory.panApplication.getDescription()
         alert.contentText = "Version ${getVersion()} build ${getBuildNumber()}." +
                 "\n\n " +
-                "\u00a9 1996-2024 Karl Dinwiddie."
+                "\u00a9 1996-2024 Karl Dinwiddie.\n\n" +
+
+                "This program is free software: you can redistribute it and/or modify \n" +
+                "it under the terms of the GNU General Public License as published by \n" +
+                "the Free Software Foundation, either version 3 of the License, or \n" +
+                "(at your option) any later version. \n" +
+                " \n" +
+                "This program is distributed in the hope that it will be useful, \n" +
+                "but WITHOUT ANY WARRANTY; without even the implied warranty of \n" +
+                "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the \n" +
+                "GNU General Public License for more details. \n" +
+                " \n" +
+                "You should have received a copy of the GNU General Public License \n" +
+                "along with this program.  If not, see <https://www.gnu.org/licenses/>."
         val stage = alert.dialogPane.scene.window as Stage
         stage.icons.add(dds!!.createFaviconImage())
         stage.scene.root.style = fxDoc.scene.root.style
+        alert.dialogPane.setPrefSize(600.0, 400.0);
         alert.showAndWait()
     }
 
